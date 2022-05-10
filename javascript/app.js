60a0cbe5841406f65f5c622cc3fafbfb2bfffe49
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2022-05-10 03:59:22 -0700

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,24 @@
+// question 1 
+function task1(){
+    alert("Hello visitors !")
+
+}
+
+
+// question 2 
+function task2(){
+alert('Error! Please enter a valid password.')
+}
+
+
+// question 3
+function task3(){
+    alert('Welcome to JS Land... \n Happy Coding !');
+
+}
+
+// question 4
+function task4(){
+alert('Welcome to JS Land')
+alert(' Happy Coding !  \n')
+}
